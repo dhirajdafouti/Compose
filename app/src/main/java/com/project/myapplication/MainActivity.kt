63dea54ef01a2bc26ee8670476236129fe8a3a17
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-04-27 22:05:51 +0530

    Compose

--- app/src/main/java/com/project/myapplication/MainActivity.kt
+++ app/src/main/java/com/project/myapplication/MainActivity.kt
@@ -3,11 +3,17 @@ package com.project.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.project.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +32,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Row {
+        Column {
+            Text(text = "Hello $name!")
+        }
+        Column {
+            Text(text = "Dhiraj$name!")
+        }
+    }
+
 }
 
 @Preview(showBackground = true)
