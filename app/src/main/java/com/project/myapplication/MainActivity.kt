61fac3615c6806ed748270f339e745aef76a93ef
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-04-28 19:03:41 +0530

    Compose

--- app/src/main/java/com/project/myapplication/MainActivity.kt
+++ app/src/main/java/com/project/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -18,6 +19,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
@@ -37,7 +39,7 @@ class MainActivity : ComponentActivity() {
                 Surface(color = MaterialTheme.colors.background) {
                     // Greeting("Android")
                     //CardGreeting()
-                  //  MessageCard(Message("Android", "Hey i am new to jet packcompose"))
+                    MessageCard(Message("Android", "Jet Pack Compose"))
                 }
             }
 
@@ -88,34 +90,41 @@ fun DefaultPreview() {
     MyApplicationTheme {
         // Greeting("Android")
         //  CardGreeting()
-       // MessageCard(Message("Android", "Jet Pack Compose"))
+        MessageCard(Message("Android", "Jet Pack Compose"))
     }
 }
 
 @Composable
 fun MessageCard(message: Message) {
     Row(modifier = Modifier
-        .padding(5.dp)
         .background(Color.Transparent)) {
 
         Row {
             Column {
                 Text(text = message.message,
-                    style = TextStyle.Default,
+                    color = MaterialTheme.colors.primaryVariant,
+                    style = MaterialTheme.typography.subtitle1,
                     textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .border(1.dp, MaterialTheme.colors.secondary, CircleShape),
                     fontFamily = FontFamily.Default)
                 Spacer(modifier = Modifier.size(2.dp))
                 Text(text = message.author,
-                    style = TextStyle.Default,
+                    style = MaterialTheme.typography.subtitle2,
                     textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .border(1.dp, MaterialTheme.colors.secondary, CircleShape).width(80.dp),
+                    color = MaterialTheme.colors.primaryVariant,
                     fontFamily = FontFamily.Default,
                     fontSize = TextUnit.Unspecified)
             }
         }
         Image(painterResource(R.drawable.ic_pic),
-            contentDescription = "Image",
+            contentDescription = null,
             modifier = Modifier
                 .size(40.dp)
+                .border(1.5.dp, Color.Blue, CircleShape)
                 .clip(CircleShape))
+
     }
 }
