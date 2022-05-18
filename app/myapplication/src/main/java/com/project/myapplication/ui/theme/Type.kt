d845a50526d1d91f1ffe6e59d549c9c44b853c4f
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-05-18 22:54:43 +0530

    The basic structure for basic layout tutorial

--- app/myapplication/src/main/java/com/project/myapplication/ui/theme/Type.kt
+++ app/myapplication/src/main/java/com/project/myapplication/ui/theme/Type.kt
@@ -22,7 +22,8 @@ import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
-import com.codelab.basiclayouts.R
+
+import com.project.myapplication.R
 
 private val fontFamilyKulim = FontFamily(
     listOf(
