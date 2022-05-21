da68f7f9834b6ff26caaed488b1c12400cd44cef
CodingRock-Star <dhirajdafouti2020@gmail.com>
2022-05-21 22:10:40 +0530

    The basic structure for basic layout tutorial

--- app/myapplication/src/main/java/com/project/myapplication/LayoutBasic.kt
+++ app/myapplication/src/main/java/com/project/myapplication/LayoutBasic.kt
@@ -8,7 +8,6 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
